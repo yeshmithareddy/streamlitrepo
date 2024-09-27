e7b2a99f1e1514532a4741905b085cd04acfee28
yeshmithareddy <106657393+yeshmithareddy@users.noreply.github.com>
2024-09-27 14:20:31 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,4 +5,7 @@ st.title('Machine Learning App')
 
 st.info('This is a Machine learning App')
 df=pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/refs/heads/master/penguins_cleaned.csv')
-df
+with st.expander('Data'):
+  st.write('**Raw data**')
+  df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
+  df
