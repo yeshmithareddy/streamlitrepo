ec4f46d0467d0216aeae7d690a931db683044de9
yeshmithareddy <106657393+yeshmithareddy@users.noreply.github.com>
2024-09-27 14:18:45 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,5 @@ import pandas as pd
 st.title('Machine Learning App')
 
 st.info('This is a Machine learning App')
-df=pd.readcsv('https://raw.githubusercontent.com/dataprofessor/data/refs/heads/master/penguins_cleaned.csv')
+df=pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/refs/heads/master/penguins_cleaned.csv')
 df
